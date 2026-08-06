350e19e787de325c785f6bda59c30f224e195b7e
Praveen Muttappa Sunagar <mspraveen7777@gmail.com>
2026-08-06 13:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.27%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,15 +1,13 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        n = num
-
+        n = num 
         while len(str(n)) != 1:
             res = 0
 
             while n > 0:
                 ld = n % 10
-                res += ld
-                n //= 10
-
+                res +=ld
+                n = n //10
             n = res
-
         return n
+        
